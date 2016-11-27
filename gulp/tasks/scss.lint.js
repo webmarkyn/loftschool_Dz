454697885bb346ff2d252780d2806001d827815c
Mark Baidebura <web.markyn@gmail.com>
2016-11-27 16:06:35 +0300

    added sccs.lint cfg file

--- gulp/tasks/scss.lint.js
+++ gulp/tasks/scss.lint.js
@@ -3,7 +3,9 @@
 module.exports = function() {
     $.gulp.task('scss:lint', function() {
         return $.gulp.src('./source/style/**/*.s+(a|c)ss')
-            .pipe($.gp.sassLint())
+            .pipe($.gp.sassLint({
+                configFile: 'sasslintcfg.yml'
+            }))
             .pipe($.gp.sassLint.format())
             .pipe($.gp.sassLint.failOnError())
     })
