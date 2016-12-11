5f79d4383860c4d79c5654d23b82e58955825455
Mark <web.markyn@gmail.com>
2016-12-11 15:58:28 +0300

    adaptive

--- source/js/app.js
+++ source/js/app.js
@@ -1,8 +1,24 @@
-(function() {
-  'use strict';
+var myModule = (function () {
+    
+    //Инициализация модуля
+    var init = function () {
+        _setUpListners();
+    };
+    
+    //Прослушка событий
+    var _setUpListners = function () {
+    	_flipElem('authorize', 'flipWrap');
+    	_flipElem('backToMain', 'flipWrap');
+    }
 
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
-  
+    // Flip func
+    var _flipElem = function(buttonId, containerID) {
+    	$('#' + buttonId).on("click", function(e){e.preventDefault();$("#" + containerID).toggleClass("hover");})
+    };
+
+    //Публичные методы
+    return {
+        init: init
+    }
 })();
+myModule.init();
