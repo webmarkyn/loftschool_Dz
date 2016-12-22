05b894f288c76839b5b981f975a532a00980ac37
Mark <web.markyn@gmail.com>
2016-12-22 13:16:51 +0300

    slider done

--- source/js/app.js
+++ source/js/app.js
@@ -15,39 +15,77 @@ var flipper = (function() {
 
 //SLIDER
 var slider = (function () {
-    var counter = 1,
+    var counter = 0,
         duration = 300,
+        sliderPreviewContainer = $('.works__current-slide'),
+        sliderInfoContainer = $('.works__slide-info'),
         flag = true;
 
     var moveSlide = function (container, direction) {
-        var items = container.find('.slider__item'),
+        var items = container.find('.slider-nav__item'),
             activeItem = items.filter('.active'),
-            direction = direction == 'down' ? 100 : -100;
+            animDirection = direction == 'down' ? 100 : -100;
 
 
 
-        console.log(items.length);
 
         if (counter >= items.length) {
             counter = 0;
         }
-        if(counter <= -1 * items.length) {
-            counter = 0;
+        if (counter == -1) {
+            counter = 3;
         }
 
+
+
         var requeItem = items.eq(counter);
-        console.log(counter);
+
+
         activeItem.animate({
-            'top': direction + '%'
+            'top': animDirection + '%'
         }, duration);
 
         requeItem.animate({
             'top': '0'
         }, duration, function () {
-            activeItem.removeClass('active').css('top', -direction + '%');
+            activeItem.removeClass('active').css('top', -animDirection + '%');
             $(this).addClass('active');
             flag = true;
         });
+    };
+
+    var moveSlidePreview = function () {
+        var items = sliderPreviewContainer.find('.current-slide__item'),
+            activeItem = items.filter('.active'),
+            requeItem = items.eq(counter);
+
+        activeItem.animate({
+            'left': '-200%'
+        }, duration);
+
+        requeItem.animate({
+            'left': '0'
+        }, duration, function () {
+            activeItem.removeClass('active').css('left', '100%');
+            $(this).addClass('active');
+        })
+    };
+
+    var moveSlideinfo = function () {
+      var items =  sliderInfoContainer.find('.slider-info__item'),
+          activeItem = items.filter('.active'),
+          requeItem = items.eq(counter);
+
+        activeItem.animate({
+            'left': '-150%'
+        }, duration);
+
+        requeItem.animate({
+            'left': '0'
+        }, duration, function () {
+            activeItem.removeClass('active').css('left', '150%');
+            $(this).addClass('active');
+        })
     };
 
     var activateSlider = function (container, direction) {
@@ -56,14 +94,18 @@ var slider = (function () {
 
             if(flag == true) {
                 flag = false;
-                moveSlide($(this), direction);
-                console.log($(this).siblings('.js-slider-down'));
                 if (direction == 'down') {
                     counter--;
-                    moveSlide($(this).siblings('.js-slider-down'), 'down');
+                    moveSlide($(this).siblings('.js-slider-up'), 'up');
+                    moveSlide($(this), direction);
+                    moveSlidePreview();
+                    moveSlideinfo();
                 }else {
                     counter++;
-                    moveSlide($(this).siblings('.js-slider-up'), 'down');
+                    moveSlide($(this).siblings('.js-slider-down'), 'down');
+                    moveSlide($(this), direction);
+                    moveSlidePreview();
+                    moveSlideinfo();
                 }
             }
         })
